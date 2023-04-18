51a602ba914fa3f23535c1d09bd49f431b387769
Peter Kouassi <peterckouassi@gmail.com>
2023-04-18 11:40:26 -0500

    new file:   .vscode/settings.json 	modified:   Microscope Control and Capture/Capture.py 	new file:   Microscope Control and Capture/__pycache__/Raspi_Stage_Control.cpython-310.pyc

--- Microscope Control and Capture/Capture.py
+++ Microscope Control and Capture/Capture.py
@@ -1,38 +1,58 @@
-import cv2 as cv
+import time
+
+import torch
 import numpy as np
-import os
-import Raspi_Stage_Control as RSC
+from torchvision import models, transforms
 
-# Index camera and open window
-capture = cv.VideoCapture(0)
-cv.namedWindow("Mobile Microscope V2")
-scan_number = 0
-dirname = "/Users/peter/Desktop/Python Microscope/image"
+import cv2
+from PIL import Image
 
-# Read frames from video capture
-while True: #Begin while loop if feed is coming in from the camera
-    ret, frame = capture.read()
-    if not ret:
-        print("Failed to read frame")
-        break
-    cv.imshow("Mobile Microscope V2", frame)
+torch.backends.quantized.engine = 'qnnpack'
 
-    #Allow window to open and remain open until key is pressed 27=escape key 32= space key
-    k = cv.waitKey(1) & 0xFF
-    if k%256 == 27:
-        print("Escape hit, closing the window")
-        break
-    if k%256 == 32:
-        print("Space bar hit, taking a picture")
-        image_name = "TB_SSM_Scan_{}.jpg".format(scan_number)
-        cv.imwrite(os.path.join(dirname, image_name), frame) 
-        scan_number += 1
-    if k == ord('x'):
-        RSC.translate_axis('x')
-    if k == ord('y'):
-        RSC.translate_axis('y')
-    
-    
-capture.release()
+cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 224)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 224)
+cap.set(cv2.CAP_PROP_FPS, 36)
 
-cv.destroyAllWindows()
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+net = models.quantization.mobilenet_v2(pretrained=True, quantize=True)
+# jit model to take it from ~20fps to ~30fps
+net = torch.jit.script(net)
+
+started = time.time()
+last_logged = time.time()
+frame_count = 0
+
+with torch.no_grad():
+    while True:
+        # read frame
+        ret, image = cap.read()
+        if not ret:
+            raise RuntimeError("failed to read frame")
+
+        # convert opencv output from BGR to RGB
+        image = image[:, :, [2, 1, 0]]
+        permuted = image
+
+        # preprocess
+        input_tensor = preprocess(image)
+
+        # create a mini-batch as expected by the model
+        input_batch = input_tensor.unsqueeze(0)
+
+        # run model
+        output = net(input_batch)
+        # do something with output ...
+
+        # log model performance
+        frame_count += 1
+        now = time.time()
+        if now - last_logged > 1:
+            print(f"{frame_count / (now-last_logged)} fps")
+            last_logged = now
+            frame_count = 0
+            
