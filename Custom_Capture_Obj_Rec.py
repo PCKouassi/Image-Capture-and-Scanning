8d843253c9f65f0ada78f68caf1b2890d86a8564
Peter Kouassi <peterckouassi@gmail.com>
2023-04-19 21:56:47 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    Microscope Control and Capture/randome.py -> Custom_Capture_Obj_Rec.py
    	new file:   Microscope Control and Capture/Capture_Obj_Rec.py
    	deleted:    Microscope Control and Capture/tester.py
    	deleted:    Microscope Control and Capture/try.py

--- Custom_Capture_Obj_Rec.py
+++ Custom_Capture_Obj_Rec.py
@@ -25,10 +25,21 @@ class Thread(QThread):
         self.trained_file = None
         self.status = True
         self.cap = True
+        self.scan_number = 0 
+
 
     def set_file(self, fname):
         # The data comes with the 'opencv-python' module
         self.trained_file = os.path.join(cv2.data.haarcascades, fname)
+
+    def capture(self):
+        dirname = "/Users/peter/Desktop/captures"
+        print("Taking a picture")
+        ft, image = self.cap.read()
+        image_name = "TB_SSM_Scan_{}.jpg".format(self.scan_number)
+        cv2.imwrite(os.path.join(dirname, image_name), image)
+        print("Saved image to dirname")
+        self.scan_number += 1
 
     def run(self):
         self.cap = cv2.VideoCapture(0)
@@ -69,7 +80,7 @@ class Window(QMainWindow):
         super().__init__()
         # Title and dimensions
         self.setWindowTitle("Patterns detection")
-        self.setGeometry(0, 0, 800, 500)
+        self.setGeometry(0, 0, 800, 480)
 
         # Main menu bar
         self.menu = self.menuBar()
@@ -109,8 +120,11 @@ class Window(QMainWindow):
         buttons_layout = QHBoxLayout()
         self.button1 = QPushButton("Start")
         self.button2 = QPushButton("Stop/Close")
+        self.button3 = QPushButton("Capture")
         self.button1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         self.button2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self.button3.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        buttons_layout.addWidget(self.button3)
         buttons_layout.addWidget(self.button2)
         buttons_layout.addWidget(self.button1)
 
@@ -131,7 +145,9 @@ class Window(QMainWindow):
         # Connections
         self.button1.clicked.connect(self.start)
         self.button2.clicked.connect(self.kill_thread)
+        self.button3.clicked.connect(self.capture_button)
         self.button2.setEnabled(False)
+        self.button3.setEnabled(False)
         self.combobox.currentTextChanged.connect(self.set_model)
 
     @Slot()
@@ -141,6 +157,7 @@ class Window(QMainWindow):
     @Slot()
     def kill_thread(self):
         print("Finishing...")
+        self.button3.setEnabled(False)
         self.button2.setEnabled(False)
         self.button1.setEnabled(True)
         self.th.cap.release()
@@ -153,6 +170,7 @@ class Window(QMainWindow):
     @Slot()
     def start(self):
         print("Starting...")
+        self.button3.setEnabled(True)
         self.button2.setEnabled(True)
         self.button1.setEnabled(False)
         self.th.set_file(self.combobox.currentText())
@@ -161,6 +179,10 @@ class Window(QMainWindow):
     @Slot(QImage)
     def setImage(self, image):
         self.label.setPixmap(QPixmap.fromImage(image))
+    
+    @Slot()
+    def capture_button(self):
+        self.th.capture()
 
 
 if __name__ == "__main__":
