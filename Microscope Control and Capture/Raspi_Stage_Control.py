ef82ca12da180dc67d087cea3229be1655ce0a2f
Peter Kouassi <peterckouassi@gmail.com>
2023-04-22 17:37:23 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Microscope Control and Capture/Raspi_Stage_Control.py

--- Microscope Control and Capture/Raspi_Stage_Control.py
+++ Microscope Control and Capture/Raspi_Stage_Control.py
@@ -1,80 +1,26 @@
- import sys
 import RPi.GPIO as GPIO
 import time
-import encodings
-import PyQt5
-from PyQt5 import QtCore, QtGui, QtWidgets
-from gui_v2 import Ui_MainWindow
-from PyQt5.QtCore import QTimer
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+control_pins = [26,19,13,6]
+for pin in control_pins:
+  GPIO.setup(pin, GPIO.OUT)
+  GPIO.output(pin, 0)
 
 
-#STEP(PUL) PINS
-# stepper A
-GPIO.setup(17,GPIO.OUT)
-GPIO.output(17, False)
-# stepper B
-GPIO.setup(5,GPIO.OUT)
-GPIO.output(5, False)
-
-#DIR PINS
-# stepper A
-GPIO.setup(18,GPIO.OUT)
-GPIO.output(18, False)
-# stepper B
-GPIO.setup(6,GPIO.OUT)
-GPIO.output(6, False)
-
-#ENABLE PINS
-# stepper A
-GPIO.setup(27,GPIO.OUT)
-GPIO.output(27, True)
-# stepper B
-GPIO.setup(19,GPIO.OUT)
-GPIO.output(19, True)
-
-x_delay = 0.002  
-
-    
-class gui(Ui_MainWindow):
-    def __init__(self, dialog):
-        Ui_MainWindow.__init__(self)
-        self.setupUi(dialog)
-        self.button_B.clicked.connect(self.moveB)
-        self.button_A.clicked.connect(self.moveA)
-        self.timer= QTimer()
-
-    
-    def moveB(self): #as long as button B is kept pressed, this motor should keep running
-        GPIO.output(19, True) #EN
-        GPIO.output(6, True)  #DIR
-        GPIO.output(5, False)
-        time.sleep(0.00001)
-        GPIO.output(5, True)
-          
-    def moveA(self): #by one press of button A, this motor runs continuously
-
-        GPIO.output(27, True) #EN
-        GPIO.output(18, False) #DIR
-        def f():
-            GPIO.output(17, False)
-            time.sleep(0.002)
-            GPIO.output(17, True)
-        self.timer.timeout.connect(f)
-        self.timer.start(2) #2msec
-
-
-#    def stop(self):
-#        GPIO.output(27, False)
-#        self.timer.stop()
-
-               
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QMainWindow()
-    program = gui(dialog)
-    dialog.show()
-    sys.exit(app.exec_())
-     
+halfstep_seq = [
+  [1,0,0,0],
+  [1,1,0,0],
+  [0,1,0,0],
+  [0,1,1,0],
+  [0,0,1,0],
+  [0,0,1,1],
+  [0,0,0,1],
+  [1,0,0,1]
+]
+for i in range(512):
+  for halfstep in range(8):
+    for pin in range(4):
+      GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+    time.sleep(0.001)
+GPIO.cleanup()
